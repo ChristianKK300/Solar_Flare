031e925dabf509c5b5ff0650324dfcae122e5e79
hronoses <hronos7@gmail.com>
2018-02-21 22:43:03 -0500

    FixBugs

--- strugarek_nonlocal.py
+++ strugarek_nonlocal.py
@@ -6,8 +6,6 @@ class Strugarek(object):
     def __init__(self, shape):
         self.shape = shape
         self.cells = np.zeros(shape, dtype=float)
-        # self.set_initial_cell_values()
-        self.load_initial_cell_values()
         self.dA = np.zeros(self.shape)
         self.de = np.zeros(self.shape)
 
@@ -22,6 +20,7 @@ class Strugarek(object):
         self.extraction = False
         self.random_threshold = False
         self.conservative = True
+        self.local = True
 
         self.flare_durations = []
         self.flare_peak_en = []
@@ -32,15 +31,22 @@ class Strugarek(object):
 
         self.kernel = np.array([[0, -0.25, 0], [-0.25, 1, -0.25], [0, -0.25, 0]])
         self.iteration = 0
-        self.r = self.get_neighbors()
+        self.r = self.get_neighbors(self.shape[0])  # assuming that the lattice is square
 
-    def set_initial_cell_values(self):
-        start = 1
-        for i in range(24):
-            self.cells[i:-i, i:-i] += start
+        self.load_initial_cell_values()
+
 
     def load_initial_cell_values(self):
-        self.cells = np.load('data/cells_at_soc.npy')
+        try:
+            init_values = np.load('data/cells_at_soc.npy')
+            if init_values.shape == self.shape:
+               self.cells = init_values
+            else:
+               self.iteration_change_drive = 10 ** 8
+               print("Initial data is not loaded !!!!!!!!!!!!!")
+        except:
+            print("Initial data is not loaded !!!!!!!!!!!!!")
+            pass
 
     def calculate_dA_conv(self, cells):
         # convolution = signal.fftconvolve(cells, self.kernel, mode='valid')
@@ -51,85 +57,86 @@ class Strugarek(object):
         self.dA[1:-1, 1:-1] = signal.convolve(cells, self.kernel, mode='valid')
 
 
-    def find_peaks(self, cells, stochastic_threshold=False):
+    def find_peaks(self, cells):
         # returns cells indices with dA > Zc
         # self.calculate_dA(cells)
         self.calculate_dA_conv(cells)
         # self.calculate_dA_conv2(cells)
 
-        if stochastic_threshold:
+        if self.random_threshold:
             Zc_stochastic = np.random.normal(self.Zc, self.sigmaZc, size=self.shape)
             return np.abs(self.dA) > Zc_stochastic
         return np.abs(self.dA) > self.Zc
 
-    def redistribute_strugarek(self, cell_xy, peak_sign, random_redistribution=False, extraction=False, conservative=True):
+    def redistribute_LuH(self, cell_xy, peak_sign):
+        x, y = cell_xy
+        self.cells[x, y] -= peak_sign * self.Zc * 0.8
+        self.cells[x + 1, y] += peak_sign * self.Zc * 0.2
+        self.cells[x - 1, y] += peak_sign * self.Zc * 0.2
+        self.cells[x, y + 1] += peak_sign * self.Zc * 0.2
+        self.cells[x, y - 1] += peak_sign * self.Zc * 0.2
+
+    def redistribute_strugarek(self, cell_xy, peak_sign):
             x, y = cell_xy
             Z = peak_sign * self.Zc
             r0 = 1
-            if not conservative:
+            if not self.conservative:
                 r0 = np.random.uniform(self.Dnc, 1)
             # print np.abs(self.dA[x, y]) - self.Zc
-            if extraction:
-                print(np.abs(self.dA[x, y]))
+            if self.extraction:
+                # print(np.max(np.abs(self.dA[x, y])))
                 Z = peak_sign * np.random.uniform(np.abs(self.dA[x, y]) - self.Zc, self.Zc)
 
             c = r0 * 0.2
-            if random_redistribution:
-                r = np.random.rand(4)
-                R = np.sum(r)
-                self.cells[x, y] -= Z * 0.8
-                self.cells[x + 1, y] += (r[0] / R) * Z * 4 * c
-                self.cells[x - 1, y] += (r[1] / R) * Z * 4 * c
-                self.cells[x, y + 1] += (r[2] / R) * Z * 4 * c
-                self.cells[x, y - 1] += (r[3] / R) * Z * 4 * c
+            neighbours = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+            number_of_neighbors = 4
+            if not self.local:
+                neighbours = []
+                r = np.random.choice(np.arange(1, int(self.shape[0]/2) - 1, dtype=int), size=number_of_neighbors, p=self.r)
+                phi = np.random.choice(np.arange(360, dtype=int), size=number_of_neighbors)
+                for i in range(number_of_neighbors):
+                    x_1, y_1 = int(np.rint(r[i] * np.cos(np.deg2rad(phi[i])))), int(
+                        np.rint(r[i] * np.sin(np.deg2rad(phi[i]))))
+                    neighbours.append([x_1, y_1])
 
-            else:
-                self.cells[x, y] -= Z * 0.8
-                self.cells[x + 1, y] += Z * c
-                self.cells[x - 1, y] += Z * c
-                self.cells[x, y + 1] += Z * c
-                self.cells[x, y - 1] += Z * c
+            self.cells[x, y] -= Z * 0.8
+            for x1, y1 in neighbours:
+                if self.random_redistribution:
+                    r = np.random.rand(4)
+                    R = np.sum(r)
+                    try:
+                        self.cells[x + x1, y + y1] += (r[0] / R) * Z * 4 * c
+                    except:
+                        pass
+                else:
+                    try:
+                        self.cells[x + x1, y + y1] += Z * c
+                    except:
+                        pass
 
-    def get_neighbors(self):
-        r = 1 / (np.arange(1, 23, dtype=float) ** 2)
-        r *= 1/np.sum(r)  # normalization
+
+    def get_neighbors(self, size):
+        r = 1 / (np.arange(1, int(size/2) - 1, dtype=float) ** 2)
+        r *= 1 / np.sum(r)  # normalization
         return r
 
 
 
-    def redistribute_nonlocal(self, cell_xy, peak_sign):
-            x, y = cell_xy
-            Z = peak_sign * self.Zc
-            c = 0.2
-            # for each cell to redistribute I need two numbers (x,y)
-            number_of_neighbors = 4
-            r = np.random.choice(np.arange(1, 23, dtype=int), size=number_of_neighbors, p=self.r)
-            phi = np.random.choice(np.arange(360, dtype=int), size=number_of_neighbors)
-
-            self.cells[x, y] -= Z * (1 - c)
-            for i in range(number_of_neighbors):
-                x1, y1 = int(np.rint(r[i] * np.cos(np.deg2rad(phi[i])))), int(np.rint(r[i] * np.sin(np.deg2rad(phi[i]))))
-                # try in case if neighbor out of a lattice
-                try:
-                    self.cells[x + x1, y + y1] += Z * c
-                except:
-                    pass
-
     def evolve(self):
-        peaks = self.find_peaks(self.cells, stochastic_threshold=self.random_threshold)
+        peaks = self.find_peaks(self.cells)
         if np.count_nonzero(peaks):
             peaks_sign = np.sign(self.dA * peaks)
             cells_to_redistribute = np.vstack(np.nonzero(peaks)).T
             for cell in cells_to_redistribute:
-                # self.redistribute_strugarek(cell, peaks_sign[cell[0], cell[1]],
-                #                             random_redistribution=self.random_redistribution,
-                #                             extraction=self.extraction,
-                #                             conservative=self.conservative)
-                self.redistribute_nonlocal(cell, peaks_sign[cell[0], cell[1]])
+                if self.iteration > self.iteration_change_drive:
+                    self.redistribute_strugarek(cell, peaks_sign[cell[0], cell[1]])
+                else:
+                    self.redistribute_LuH(cell, peaks_sign[cell[0], cell[1]])
 
             self.de = 0.8 * self.Zc ** 2 * (2 * np.abs(self.dA) / self.Zc - 1) * peaks
             # self.de = 0.8 * self.Zc * (np.abs(self.dA)) * peaks
             E = np.sum(self.de)
+            # print(E)
             self.energies.append(E)
             self.avalanche_time += 1
         else:
@@ -165,8 +172,6 @@ class Strugarek(object):
 
 
 if __name__ == '__main__':
-    # import time
-    # from tqdm import tqdm
     #params
     # size of a lattice
     # threshold to lunch avalanche
@@ -178,7 +183,7 @@ if __name__ == '__main__':
 
     # strugarek variations:  random redistribution, random extraction, random threshold, conservation
 
-    # problem with random extraction, blows up. Need to tweak drive and threshold params
+
 
     params = [[0, 1, 0],   # like in a paper, [random threshold, extraction, random redistr]
               [1, 0, 0],
@@ -191,27 +196,28 @@ if __name__ == '__main__':
               ]
 
     sun = Strugarek((48, 48))
-    for p in [params[-1]]:
+
+
+
+    for p in [params[0]]:
         print(p)
         sun.random_threshold = p[0]
         sun.random_redistribution = p[2]
         sun.extraction = p[1]
         sun.conservative = True
+        sun.local = False
+        sun.Dnc = 0.1
 
-        iterations = 10**7
+        iterations = 10**6
+        # WARNING check iteration_change_drive and be sure that it is larger then LuHam needs to settle to SOC
 
-        # t = time.time()
-        # for i in tqdm(range(iterations)):
         for i in range(iterations):
             sun.evolve()
             if not i % 1000:
                 print(float(i)/iterations * 100),
                 print('%')
-            # print sun.get_neighbors()
 
-        # np.save('data/cells_at_soc', sun.cells)
-
-        # print("Simulation time: " + str(time.time() - t))
-        filename = 'strugarek_nonlocal_con1_'+str(p[0])+str(p[1])+str(p[1])+'_iter_' + str(iterations)
+        filename = 'strugarek_local=' + str(sun.local) + '_cons=' + str(sun.conservative) \
+                   + '_Dnc=' + str(sun.Dnc) + '_' + str(p[0]) + str(p[1]) + str(p[1]) + '_iter=' + str(iterations)
         sun.save_data(filename)
 
